39cf2cabee294c6e39ceb08b0bc7757ec7a6c894
Slash750 <116109011+Slash750@users.noreply.github.com>
2022-10-19 16:03:32 +0300

    Project & Stage Classes
    Modification on Project class.
    Creation of Stage class.

---
+++ SWE316Homework/src/Entities/Stage.java
@@ -0,0 +1,67 @@
+package Entities;
+
+import java.util.Date;
+
+public class Stage {
+	private char changeIndicator;
+	private Date date;
+	private int documentNumber;
+	private String fieldName;
+	private int newValue;
+	private String objectValue;
+	private int oldvalue;
+	private int textFlag;
+	private Date time;
+	
+	public Stage(char changeIndicator, Date date, int documentNumber,
+			String fieldName, int newValue, String objectValue,
+			int oldvalue, int textFlag, Date time) {
+		
+		this.changeIndicator = changeIndicator;
+		this.date = date;
+		this.documentNumber = documentNumber;
+		this.fieldName = fieldName;
+		this.newValue = newValue;
+		this.objectValue = objectValue;
+		this.oldvalue = oldvalue;
+		this.textFlag = textFlag;
+		this.time = time;
+		
+	}
+	
+	public char getChangeIndicator() {
+		return changeIndicator;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public int getDocumentNumber() {
+		return documentNumber;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public int getNewValue() {
+		return newValue;
+	}
+
+	public String getObjectValue() {
+		return objectValue;
+	}
+
+	public int getOldvalue() {
+		return oldvalue;
+	}
+
+	public int getTextFlag() {
+		return textFlag;
+	}
+
+	public Date getTime() {
+		return time;
+	}
+}
