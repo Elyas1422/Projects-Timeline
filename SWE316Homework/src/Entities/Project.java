66f2076c8170da5c43d4a82884c34440848280a2
Elyas Almubarak <79568124+Elyas1422@users.noreply.github.com>
2022-10-19 16:23:33 +0300

    Squashed commit of the following:
    commit 39cf2cabee294c6e39ceb08b0bc7757ec7a6c894
    Author: Slash750 <116109011+Slash750@users.noreply.github.com>
    Date:   Wed Oct 19 16:03:32 2022 +0300
    
        Project & Stage Classes
    
        Modification on Project class.
        Creation of Stage class.

--- SWE316Homework/src/Entities/Project.java
+++ SWE316Homework/src/Entities/Project.java
@@ -1,5 +1,67 @@
 package Entities;
 
+import java.util.Date;
+
 public class Project {
-    int x,y;
+	private Date changedOn;
+	private Date createdOn;
+	private String currency;
+	private String customer;
+	private String customerProjectID;
+	private Date endDate;
+	private String nodeID;
+	private int stage;
+	private Date startDate;
+	
+	public Project(Date changedOn, Date createdOn, String currency, 
+			String customer, String customerProjectID, Date endDate,
+			String nodeID, int stage, Date startDate){
+		
+		this.changedOn = changedOn;
+		this.createdOn = createdOn;
+		this.currency = currency;
+		this.customer = customer;
+		this.customerProjectID = customerProjectID;
+		this.endDate = endDate;
+		this.nodeID = nodeID;
+		this.stage = stage;
+		this.startDate = startDate;
+	}
+	
+	public Date getChangedOn() {
+		return changedOn;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public String getCustomer() {
+		return customer;
+	}
+
+	public String getCustomerProjectID() {
+		return customerProjectID;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public String getNodeID() {
+		return nodeID;
+	}
+
+	public int getStage() {
+		return stage;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+	
 }
